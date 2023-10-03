45a66a14af23103aa645f5158aece5f2ca50a8ac
saifffff <angryn1nj4@gmail.com>
2023-10-03 19:49:36 +0530

    interacting with a contract deployed on the testnet

---
+++ interaction.js
@@ -0,0 +1,106 @@
+const {ethers,JsonRpcProvider} = require('ethers');
+const provider = new JsonRpcProvider(`https://sepolia.infura.io/v3/3c7eac9b2dd149b8ab3abf0ae6cc321b`); // infura sepolia testnet integration
+
+const conAddress = "0x41e0d8031a72b9bd084a4c152cd22ba366e64965"; // this is the address of the deployed contract
+// to interact with the contract we also need the contract ABI (abstract binary interface)
+const abi = [
+	{
+		"inputs": [],
+		"name": "sendEth",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_user",
+				"type": "address"
+			}
+		],
+		"name": "sendEth",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_bal",
+				"type": "uint256"
+			}
+		],
+		"name": "setBalance",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "contractBal",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_user",
+				"type": "address"
+			}
+		],
+		"name": "getAddressBal",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+const contractInteraction =async()=>{
+    const WalletCon = new ethers.Contract(conAddress,abi,provider);
+    const walletName = await WalletCon.name();
+    console.log("Currently interacting with wallet : ",walletName);
+}
+
+contractInteraction();
